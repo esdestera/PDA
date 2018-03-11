0cbc8454fbeaaf5df7bbd8730f9f83062ec3fc4b
esdestera <scurtu_estera@yahoo.com>
2018-03-11 10:10:07 +0200

    Update Consumer.java

--- ProdCons/src/Consumer.java
+++ ProdCons/src/Consumer.java
@@ -2,26 +2,23 @@ import java.util.Queue;
 
 public class Consumer implements Runnable {
 	Queue<Integer> queue;
-	
-	
+
 	public Consumer(Queue<Integer> queue) {
 		super();
 		this.queue = queue;
 	}
 
-
 	public void run() {
-		while(true){
-		if(!queue.isEmpty()) {
-			synchronized (queue) {
-				int number = queue.remove();
-				System.out.println("Consumed " + number);
-			}
-		}
-		else {
+		while (true) {
+			if (!queue.isEmpty()) {
+				synchronized (queue) {
+					int number = queue.remove();
+					System.out.println("Consumed " + number);
+				}
+			} else {
 				System.out.println("There are not elements in the queue");
 			}
-	}
+		}
 	}
 }
 
